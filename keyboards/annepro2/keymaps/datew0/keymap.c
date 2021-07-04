8b1fcfca20bd0c9cf84271f267347b103df004d9
datew0 <local@localdomain.local>
2021-07-05 00:24:16 +0300

    Update keymap.c

--- keyboards/annepro2/keymaps/datew0/keymap.c
+++ keyboards/annepro2/keymaps/datew0/keymap.c
@@ -11,38 +11,24 @@ enum my_layers {
   _FN1L,
   _FN2L
 };
- const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [_BL] = LAYOUT(
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, ACTION_TAP_DANCE_DUAL_ROLE(KC_BSLS, _MACL),
-    LT(_FN1L,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(_FN1L,KC_LEFT), LT(_FN2L,KC_DOWN), RCTL_T(KC_RGHT)
-),
- [_MACL] = LAYOUT(
-    _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
-    _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, ACTION_TAP_DANCE_DUAL_ROLE(KC_BSLS, _BL),
-    _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
-    _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
-    _____, KC_LALT, KC_LGUI, _____, _____, _____, _____, _____
-)
-/*
- [_FN1L] = LAYOUT(
-    _____, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _____, _____, _____, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, _____, _____,
-    MO(_FN2L), _____, KC_UP, _____, _____, _____, _____, _____, _____, _____, KC_PSCR, KC_HOME, KC_END, _____,
-    _____, KC_LEFT, KC_DOWN, KC_RGHT, _____, _____, _____, _____, _____, _____, KC_PGUP, KC_PGDN, _____,
-    _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_INS, KC_DEL, _____,
-    _____, _____, _____, _____, _____, MO(_FN1L), MO(_FN2L), _____
- ),
- [_FN2L] = LAYOUT(
-    _____, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _____, _____, _____, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, _____, _____,
-    MO(_FN2L), _____, KC_UP, _____, _____, _____, _____, _____, _____, _____, KC_PSCR, KC_HOME, KC_END, _____,
-    _____, KC_LEFT, KC_DOWN, KC_RGHT, _____, _____, _____, _____, _____, _____, KC_PGUP, KC_PGDN, _____,
-    _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_INS, KC_DEL, _____,
-    _____, _____, _____, _____, _____, MO(_FN1L), MO(_FN2L), _____
- )
- */
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BL]  = KEYMAP(/* Base */
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, ACTION_TAP_DANCE_DUAL_ROLE(KC_BSLS, _MACL),
+        LT(_CAPS_LAYER, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_TRNS, KC_TRNS, KC_RALT, KC_TRNS),
+    [_MACL]   = KEYMAP(/* Mac */
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, ACTION_TAP_DANCE_DUAL_ROLE(KC_BSLS, _BL),
+        LT(_CAPS_LAYER, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_TRNS, KC_TRNS, KC_RALT, KC_TRNS),
 };
+
+
+
 const uint16_t keymaps_size = sizeof(keymaps);
 
 
