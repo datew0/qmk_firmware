94153057e1c7a20cbf080d4449b88abbe4730490
datew0 <datew0@local.localdomain>
2021-07-19 01:08:08 +0300

    Some Mac Improvements

--- keyboards/annepro2/keymaps/datew0/config.h
+++ keyboards/annepro2/keymaps/datew0/config.h
@@ -1,9 +1,3 @@
 #pragma once
 
-#define TAPPING_TOGGLE 3
-
 #define TAPPING_TERM 175
-
-// Some programs will not register TAP keys properly if they are triggered
-// too quickly.
-#define TAP_CODE_DELAY 70
--- keyboards/annepro2/keymaps/datew0/keymap.c
+++ keyboards/annepro2/keymaps/datew0/keymap.c
@@ -54,7 +54,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(_MEDIA_LAYER, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(_HW_LAYER, KC_BSLS),
         LT(_NAV_LAYER, KC_BSPC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOT, KC_ENT,
         TD(TD_SHIFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
-        KC_LCTL, KC_LALT, KC_LGUI, LT(_NAV_LAYER, KC_SPC), TD(TD_MAC_WIN_FN), KC_LEFT, KC_DOWN, KC_RIGHT),
+        KC_LALT, KC_LCTL, KC_LGUI, LT(_NAV_LAYER, KC_SPC), TD(TD_MAC_WIN_FN), KC_LEFT, KC_DOWN, KC_RIGHT),
 
     [_NAV_LAYER] = KEYMAP(/* Navigation */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
