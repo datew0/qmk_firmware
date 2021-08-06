dbda196e8a7cbf2a3c52504bc573aa45d465cf5f
datew0 <datew0@local.localdomain>
2021-08-06 20:30:21 +0300

    Update keymap.c

--- keyboards/annepro2/keymaps/datew0/keymap.c
+++ keyboards/annepro2/keymaps/datew0/keymap.c
@@ -26,7 +26,6 @@ typedef union {
 
 enum my_tapdances{
   TD_GRAVE_ESC,
-  TD_SHIFT_CAPS,
   TD_MAC_WIN_FN
 };
 
@@ -47,14 +46,14 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD(TD_GRAVE_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
         LT(_MEDIA_LAYER, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(_HW_LAYER, KC_BSLS),
         LT(_NAV_LAYER, KC_BSPC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOT, KC_ENT,
-        TD(TD_SHIFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT, LT(_NAV_LAYER, KC_SPC), TD(TD_MAC_WIN_FN), KC_LEFT, KC_DOWN, KC_RIGHT),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, TD(TD_MAC_WIN_FN), KC_LEFT, KC_DOWN, KC_RIGHT),
     [_MAC_LAYER] = KEYMAP(/* Mac */
         TD(TD_GRAVE_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
         LT(_MEDIA_LAYER, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(_HW_LAYER, KC_BSLS),
         LT(_NAV_LAYER, KC_BSPC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOT, KC_ENT,
-        TD(TD_SHIFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
-        KC_LALT, KC_LCTL, KC_LGUI, LT(_NAV_LAYER, KC_SPC), TD(TD_MAC_WIN_FN), KC_LEFT, KC_DOWN, KC_RIGHT),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
+        KC_LALT, KC_LCTL, KC_LGUI, KC_SPC, TD(TD_MAC_WIN_FN), KC_LEFT, KC_DOWN, KC_RIGHT),
 
     [_NAV_LAYER] = KEYMAP(/* Navigation */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -215,6 +214,5 @@ void mwfn_reset(qk_tap_dance_state_t* state, void* user_data) {
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_GRAVE_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRAVE),
-  [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPSLOCK),
   [TD_MAC_WIN_FN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mwfn_finished, mwfn_reset)
 };
